6fa7ccf5c67bcc736c0f5bac83cd6d464b07a85b
MER-C <MER-C@users.noreply.github.com>
2013-02-08 10:16:20 +0000

    *Debork getCategoryMembers() *Move tests to a place where NetBeans can find the JUnit package *Add unit tests

---
+++ test/org/wikipedia/WikiUnitTest.java
@@ -0,0 +1,53 @@
+package org.wikipedia;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *  Unit tests for Wiki.java
+ *  @author MER-C
+ */
+public class WikiUnitTest
+{
+    private static Wiki enWiki, deWiki;
+    
+    public WikiUnitTest()
+    {
+    }
+    
+    /**
+     *  Initialize wiki objects.
+     */
+    @BeforeClass
+    public static void setUpClass()
+    {
+        enWiki = new Wiki("en.wikipedia.org");
+        enWiki.setMaxLag(-1);
+        deWiki = new Wiki("de.wikipedia.org");
+        deWiki.setMaxLag(-1);
+    }
+    
+    @Test
+    public void namespace() throws Exception
+    {
+        assertEquals("NS: en, category", Wiki.CATEGORY_NAMESPACE, enWiki.namespace("Category:CSD"));
+        assertEquals("NS: main ns fail", Wiki.MAIN_NAMESPACE, enWiki.namespace("Star Wars: The Old Republic"));
+        assertEquals("NS: main ns fail2", Wiki.MAIN_NAMESPACE, enWiki.namespace("Some Category: Blah"));
+        assertEquals("NS: i18n fail", Wiki.CATEGORY_NAMESPACE, deWiki.namespace("Kategorie:Begriffsklärung"));
+        // assertEquals("NS: mixed i18n", Wiki.CATEGORY_NAMESPACE, deWiki.namespace("Category:Begriffsklärung"));
+    }
+    @Test
+    public void namespaceIdentifier() throws Exception
+    {
+        assertEquals("NSIdentifier: wrong identifier", "Category", enWiki.namespaceIdentifier(Wiki.CATEGORY_NAMESPACE));
+        assertEquals("NSIdentifier: i18n fail", "Kategorie", deWiki.namespaceIdentifier(Wiki.CATEGORY_NAMESPACE));
+    }
+    
+    @Test
+    public void userExists() throws Exception
+    {
+        assertTrue("I should exist!", enWiki.userExists("MER-C"));
+        assertFalse("Anon should not exist", enWiki.userExists("127.0.0.1"));
+    }
+}
