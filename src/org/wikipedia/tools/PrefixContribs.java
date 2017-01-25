2be0f9825ac42d1f35782d496cea62b47709a4e6
MER-C <MER-C@users.noreply.github.com>
2017-01-25 13:09:12 +0800

    Move non-servlet part of old PrefixContribs servlet to tools package.

---
+++ src/org/wikipedia/tools/PrefixContribs.java
@@ -0,0 +1,53 @@
+/**
+ *  @(#)PrefixContribs.java 0.01 15/10/2012
+ *  Copyright (C) 2012 - 2016 MER-C
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.wikipedia.tools;
+
+import java.io.*;
+import java.util.*;
+import javax.swing.JOptionPane;
+import org.wikipedia.*;
+
+/**
+ *  Fetches contributions from an IP address range or a group of users that
+ *  have a common prefix. Offline version of org/wikipedia/servlets/prefixcontribs.jsp.
+ *  @author MER-C
+ *  @version 0.01
+ */
+public class PrefixContribs
+{
+    /**
+     *  Pipe the output to a HTML file.
+     *  @param args the command line arguments (none expected)
+     *  @throws IOException if a network error occurs
+     */
+    public static void main(String[] args) throws IOException
+    {
+        Wiki enWiki = new Wiki("en.wikipedia.org");
+        enWiki.setMaxLag(-1);
+        String prefix = JOptionPane.showInputDialog(null, "Enter query string");
+        if (prefix == null)
+            System.exit(0);
+        Calendar cutoff = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+        cutoff.add(Calendar.DAY_OF_MONTH, -7);
+        Wiki.Revision[] revisions = enWiki.contribs("", prefix, cutoff, null);
+        if (revisions.length == 0)
+            System.out.println("No contributions found.");
+        else
+            System.out.println(ParserUtils.revisionsToHTML(enWiki, revisions));
+    }
+}
