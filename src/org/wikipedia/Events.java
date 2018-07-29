6e00a89fa6182f148a8a3b0f10c2f1eaa0455ed9
MER-C <MER-C@users.noreply.github.com>
2018-07-29 17:36:59 +0100

    Commit this function I have lying around (computes time between revisions or log entries).

---
+++ src/org/wikipedia/Events.java
@@ -0,0 +1,55 @@
+/**
+ *  @(#)Events.java 0.01 26/06/2018
+ *  Copyright (C) 2018-20XX MER-C and contributors
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version. Additionally
+ *  this file is subject to the "Classpath" exception.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package org.wikipedia;
+
+import java.util.*;
+import java.time.*;
+
+/**
+ *  Utility class for {@link Wiki.Event}s.
+ *  @author MER-C
+ *  @version 0.01
+ */
+public class Events
+{
+    /**
+     *  Computes the time elapsed between each of a list of {@link Wiki.Event}s.
+     *  The return list has length {@code events.size() - 1}, with the first
+     *  element being the time elapsed between the first and second events in 
+     *  the input list, the second duration being the difference between the
+     *  second and third events and so forth. It is recommended you sort the 
+     *  input list first by timestamp so that the latest timestamp is first.
+     * 
+     *  @param events a list of events
+     *  @throws IllegalArgumentException if you supply less than two events
+     *  @return the time between those events
+     */
+    public static List<Duration> timeBetweenEvents(List<? extends Wiki.Event> events)
+    {
+        int size = events.size();
+        if (size < 2)
+            throw new IllegalArgumentException("You must supply two or more events to calculate times between them.");
+        List<Duration> durations = new ArrayList<>(size);
+        for (int i = 1; i < size; i++)
+            durations.add(Duration.between(events.get(i).getTimestamp(), events.get(i-1).getTimestamp()));
+        return durations;
+    }
+}
---
+++ test/org/wikipedia/EventsTest.java
@@ -0,0 +1,69 @@
+/**
+ *  @(#)EventsTest.java 0.01 29/07/2018
+ *  Copyright (C) 2018-20XX MER-C and contributors
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version. Additionally
+ *  this file is subject to the "Classpath" exception.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package org.wikipedia;
+
+import java.time.*;
+import java.util.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *  Unit tests for the {@link Events} utility class.
+ *  @author MER-C
+ */
+public class EventsTest
+{
+    private static Wiki enWiki = Wiki.createInstance("en.wikipedia.org");
+    
+    public EventsTest()
+    {
+    }
+
+    @Test
+    public void timeBetweenEvents() throws Exception
+    {
+        try
+        {
+            Events.timeBetweenEvents(Collections.emptyList());
+            fail("Cannot compute time between a list of no events.");
+        }
+        catch (IllegalArgumentException expected)
+        {
+        }
+        
+        // https://en.wikipedia.org/wiki/Wikipedia:Articles_for_deletion/HD_Brows
+        List<? extends Wiki.Event> events = enWiki.getPageHistory("Wikipedia:Articles for deletion/HD Brows", null);
+        
+        try
+        {
+            Events.timeBetweenEvents(events.subList(0, 0));
+            fail("Cannot compute time between a list of one event.");
+        }
+        catch (IllegalArgumentException expected)
+        {
+        }
+        List<Duration> result = Events.timeBetweenEvents(events);
+        assertEquals("check whether quantity is correct", events.size() - 1, result.size());
+        // check a couple of entries
+        assertEquals(Duration.ofSeconds(4880), result.get(result.size() - 2));
+        assertEquals(Duration.ofSeconds(976), result.get(result.size() - 1));
+    }
+}
