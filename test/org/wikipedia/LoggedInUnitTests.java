MER-C/wiki-java

commit 61eba0fc75c3ffd41280f0e1a3491ff6707e54ae

Use JDK cookies as opposed to baking my own.

src/org/wikipedia/Wiki.java
@@ -422,7 +422,7 @@ public class Wiki implements Serializable
     private ZoneId timezone = ZoneId.of("UTC");
 
     // user management
-    private Map<String, String> cookies = new HashMap<>(12);
+    private final CookieManager cookies = new CookieManager(null, CookiePolicy.ACCEPT_ALL);
     private User user;
     private int statuscounter = 0;
 
@@ -451,7 +451,7 @@ public class Wiki implements Serializable
     private long lastThrottleActionTime = 0;
 
     // retry count
-    private int maxtries = 2;
+    private final int maxtries = 2;
 
     // serial version
     private static final long serialVersionUID = -8745212681497643456L;
@@ -530,7 +530,7 @@ public class Wiki implements Serializable
         this.domain = domain;
         this.scriptPath = scriptPath;
         this.protocol = protocol;
-
+        
         // init variables
         // This is fine as long as you do not have parameters other than domain
         // and scriptpath in constructors and do not do anything else than super(x)!
@@ -538,6 +538,7 @@ public class Wiki implements Serializable
         // TODO: remove this
         logger.setLevel(loglevel);
         logger.log(Level.CONFIG, "[{0}] Using Wiki.java {1}", new Object[] { domain, version });
+        CookieHandler.setDefault(cookies);
         initVars();
     }
     
@@ -1078,7 +1079,7 @@ public class Wiki implements Serializable
      */
     public synchronized void logout()
     {
-        cookies.clear();
+        cookies.getCookieStore().removeAll();
         user = null;
         max = 500;
         slowmax = 50;
@@ -3496,7 +3497,6 @@ public class Wiki implements Serializable
         // then we read the image
         logurl(url2, "getImage");
         URLConnection connection = makeConnection(url2);
-        setCookies(connection);
         connection.connect();
 
         // download image to the file
@@ -3670,7 +3670,6 @@ public class Wiki implements Serializable
                 url = parseAttribute(line, "url", a);
                 logurl(url, "getOldImage");
                 URLConnection connection = makeConnection(url);
-                setCookies(connection);
                 connection.connect();
 
                 // download image to file
@@ -7021,11 +7020,7 @@ public class Wiki implements Serializable
             {
                 // connect
                 URLConnection connection = makeConnection(url);
-                connection.setConnectTimeout(CONNECTION_CONNECT_TIMEOUT_MSEC);
-                connection.setReadTimeout(CONNECTION_READ_TIMEOUT_MSEC);
-                setCookies(connection);
                 connection.connect();
-                grabCookies(connection);
 
                 // check lag and retry
                 if (checkLag(connection))
@@ -7098,10 +7093,7 @@ public class Wiki implements Serializable
             try
             {
                 URLConnection connection = makeConnection(url);
-                setCookies(connection);
                 connection.setDoOutput(true);
-                connection.setConnectTimeout(CONNECTION_CONNECT_TIMEOUT_MSEC);
-                connection.setReadTimeout(CONNECTION_READ_TIMEOUT_MSEC);
                 connection.connect();
 
                 try (OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream(), "UTF-8"))
@@ -7112,7 +7104,6 @@ public class Wiki implements Serializable
                 if (checkLag(connection))
                     return post(url, text, caller);
 
-                grabCookies(connection);
                 StringBuilder buffer = new StringBuilder(100000);
                 String line;
                 try (BufferedReader in = new BufferedReader(new InputStreamReader(
@@ -7184,10 +7175,7 @@ public class Wiki implements Serializable
                 URLConnection connection = makeConnection(url);
                 String boundary = "----------NEXT PART----------";
                 connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-                setCookies(connection);
                 connection.setDoOutput(true);
-                connection.setConnectTimeout(CONNECTION_CONNECT_TIMEOUT_MSEC);
-                connection.setReadTimeout(CONNECTION_READ_TIMEOUT_MSEC);
                 connection.connect();
 
                 // write stuff to a local buffer
@@ -7231,7 +7219,6 @@ public class Wiki implements Serializable
                     return multipartPost(url, params, caller);
 
                 // done, read the response
-                grabCookies(connection);
                 String line;
                 StringBuilder buffer = new StringBuilder(100000);
                 try (BufferedReader in = new BufferedReader(new InputStreamReader(
@@ -7315,7 +7302,13 @@ public class Wiki implements Serializable
      */
     protected URLConnection makeConnection(String url) throws IOException
     {
-        return new URL(url).openConnection();
+        URLConnection u = new URL(url).openConnection();
+        u.setConnectTimeout(CONNECTION_CONNECT_TIMEOUT_MSEC);
+        u.setReadTimeout(CONNECTION_READ_TIMEOUT_MSEC);
+        if (zipped)
+            u.setRequestProperty("Accept-encoding", "gzip");
+        u.setRequestProperty("User-Agent", useragent);
+        return u;
     }
 
     /**
@@ -7642,52 +7635,6 @@ public class Wiki implements Serializable
         if ((Boolean)protectionstate.get("cascade") == Boolean.TRUE) // can be null
             return user.isAllowedTo("editprotected");
         return true;
-    }
-
-    // cookie methods
-
-    /**
-     *  Sets cookies to an unconnected URLConnection and enables gzip
-     *  compression of returned text.
-     *  @param u an unconnected URLConnection
-     */
-    protected void setCookies(URLConnection u)
-    {
-        StringBuilder cookie = new StringBuilder(100);
-        for (Map.Entry<String, String> entry : cookies.entrySet())
-        {
-            cookie.append(entry.getKey());
-            cookie.append("=");
-            cookie.append(entry.getValue());
-            cookie.append("; ");
-        }
-        u.setRequestProperty("Cookie", cookie.toString());
-
-        // enable gzip compression
-        if (zipped)
-            u.setRequestProperty("Accept-encoding", "gzip");
-        u.setRequestProperty("User-Agent", useragent);
-    }
-
-    /**
-     *  Grabs cookies from the URL connection provided.
-     *  @param u an unconnected URLConnection
-     */
-    private void grabCookies(URLConnection u)
-    {
-        String headerName;
-        for (int i = 1; (headerName = u.getHeaderFieldKey(i)) != null; i++)
-            if (headerName.equals("Set-Cookie"))
-            {
-                String cookie = u.getHeaderField(i);
-                cookie = cookie.substring(0, cookie.indexOf(';'));
-                String name = cookie.substring(0, cookie.indexOf('='));
-                String value = cookie.substring(cookie.indexOf('=') + 1, cookie.length());
-                // these cookies were pruned, but are still sent for some reason?
-                // TODO: when these cookies are no longer sent, remove this test
-                if (!value.equals("deleted"))
-                    cookies.put(name, value);
-            }
     }
 
     // logging methods

commit a928f4183227b6081d805c6710fac71891b0a2de

Localise cookie storage to handle multiple sessions without cross-contamination of cookies. Fixes #157, a regression from 61eba0f.

src/org/wikipedia/Wiki.java
@@ -420,7 +420,7 @@ public class Wiki implements Comparable<Wiki>
     private ArrayList<Integer> ns_subpages = null;
 
     // user management
-    private final CookieManager cookies = new CookieManager(null, CookiePolicy.ACCEPT_ALL);
+    private final CookieManager cookies;
     private User user;
     private int statuscounter = 0;
 
@@ -483,7 +483,7 @@ public class Wiki implements Comparable<Wiki>
 
         logger.setLevel(loglevel);
         logger.log(Level.CONFIG, "[{0}] Using Wiki.java {1}", new Object[] { domain, version });
-        CookieHandler.setDefault(cookies);
+        cookies = new CookieManager(null, CookiePolicy.ACCEPT_ALL);
     }
 
     /**
@@ -8184,8 +8184,22 @@ public class Wiki implements Comparable<Wiki>
             tries--;
             try
             {
-                // actually make the request
-                URLConnection connection = makeConnection(url);
+                // Cookie handling should be handled locally instead of setting
+                // the system wide cookie manager to make sure a local state is
+                // saved per instance
+                // modified from https://stackoverflow.com/questions/16150089
+                // see https://github.com/MER-C/wiki-java/issues/157
+                URLConnection connection = makeConnection(url);                
+                CookieStore store = cookies.getCookieStore();
+                List<HttpCookie> cookielist = store.getCookies();
+                if (!cookielist.isEmpty())
+                {
+                    StringJoiner sb = new StringJoiner(";");
+                    for (HttpCookie cookie : cookielist)
+                        sb.add(cookie.toString());
+                    connection.setRequestProperty("Cookie", sb.toString());
+                }
+                        
                 if (isPOST)
                 {
                     connection.setDoOutput(true);
@@ -8221,6 +8235,12 @@ public class Wiki implements Comparable<Wiki>
                 }
 
                 // get the response from the server
+                Map<String, List<String>> headerFields = connection.getHeaderFields();
+                List<String> cookiesheader = headerFields.get("Set-Cookie");
+                if (cookiesheader != null)
+                    for (String cookie : cookiesheader)
+                        for (HttpCookie hc : HttpCookie.parse(cookie))
+                            store.add(null, hc);        
                 try (BufferedReader in = new BufferedReader(new InputStreamReader(
                     zipped ? new GZIPInputStream(connection.getInputStream()) : connection.getInputStream(), "UTF-8")))
                 {

test/org/wikipedia/LoggedInUnitTests.java
@@ -30,10 +30,9 @@ import static org.junit.Assert.*;
  */
 public class LoggedInUnitTests
 {
-    private static Wiki testWiki;
+    private final Wiki testWiki;
     
-    @BeforeClass
-    public static void setUpClass() throws Exception
+    public LoggedInUnitTests() throws Exception
     {
         testWiki = Wiki.createInstance("test.wikipedia.org");
         org.wikiutils.LoginUtils.guiLogin(testWiki);
@@ -41,45 +40,76 @@ public class LoggedInUnitTests
     }
     
     @Test
+    public void login() throws Exception
+    {
+        // one wiki instance = one session
+        // https://github.com/MER-C/wiki-java/issues/157
+        Wiki enWiki = Wiki.createInstance("en.wikipedia.org");
+        enWiki.getPageText("Main Page");        
+        // should still be logged in (also checks whether the cookies work in
+        // in the first place)...
+        testWiki.setAssertionMode(Wiki.ASSERT_USER);
+        testWiki.getPageText("Main Page");
+        enWiki.setAssertionMode(Wiki.ASSERT_USER);
+        try
+        {
+            enWiki.getPageText("Main Page");
+            fail("Cross-contamination between sections.");
+        }
+        catch (AssertionError expected)
+        {
+        }
+    }       
+    
+    @Test
     public void upload() throws Exception
     {
-        // ordinary upload
-        // target: https://test.wikipedia.org/wiki/File:Wiki.java_test4.jpg
-        // image: https://commons.wikimedia.org/wiki/File:PIA21465_-_North_Polar_Layers.jpg (6.2 MB)
-        // Note file size < 4 GB, so can read into an array and is large enough
-        // to require two chunks on default settings.
-        File expected = File.createTempFile("wikijava_upload1", null);
-        testWiki.getImage("PIA21465 - North Polar Layers.jpg", expected);
-        String description = "Test image. Source (PD-NASA): [[:File:PIA21465 - North Polar Layers.jpg]]. ∑∑ƒ∂ß";
-        String reason = "Testing upload. ∑∑ƒ∂ß";
-        String uploadDest = "Wiki.java test4.jpg";
-        testWiki.upload(expected, uploadDest, description, reason);
-        // verify file uploaded is identical to image
-        File actual = File.createTempFile("wikijava_upload2", null);
-        testWiki.getImage(uploadDest, actual);
-        assertArrayEquals("upload: image", Files.readAllBytes(expected.toPath()), 
-            Files.readAllBytes(actual.toPath()));
-        assertEquals("upload: description", description, testWiki.getPageText("File:" + uploadDest));
-        assertEquals("upload: reason", reason, testWiki.getTopRevision("File:" + uploadDest).getComment());
-        
-        // upload via URL
-        // target: https://test.wikipedia.org/wiki/File:Wiki.java_test5.jpg
-        // image: https://commons.wikimedia.org/wiki/File:(Tsander)_Large_Impact_Crater,_Lunar_Surface.jpg (1.55 MB)
-        description = "Test image. Source (PD-NASA): [[:File:(Tsander) Large Impact Crater, Lunar Surface.jpg]]";
-        uploadDest = "Wiki.java test5.jpg";
-        reason = "Testing upload via URL";
-        testWiki.upload(new URL("https://upload.wikimedia.org/wikipedia/commons/b/bc/%28Tsander%29_Large_Impact_Crater%2C_Lunar_Surface.jpg"), 
-            uploadDest, description, reason);
-        // verify file uploaded is identical to copied image
-        expected = File.createTempFile("wikijava_upload3", null);
-        testWiki.getImage("(Tsander) Large Impact Crater, Lunar Surface.jpg", expected);
-        actual = File.createTempFile("wikijava_upload4", null);
-        testWiki.getImage(uploadDest, actual);
-        // 1.55 MB file
-        assertArrayEquals("upload via url: image", Files.readAllBytes(expected.toPath()), 
-            Files.readAllBytes(actual.toPath()));
-        assertEquals("upload via url: description", description, testWiki.getPageText("File:" + uploadDest));
-        assertEquals("upload via url: reason", reason, testWiki.getTopRevision("File:" + uploadDest).getComment());
+        try
+        {
+            // ordinary upload
+            // target: https://test.wikipedia.org/wiki/File:Wiki.java_test4.jpg
+            // image: https://commons.wikimedia.org/wiki/File:PIA21465_-_North_Polar_Layers.jpg (6.2 MB)
+            // Note file size < 4 GB, so can read into an array and is large enough
+            // to require two chunks on default settings.
+            File expected = File.createTempFile("wikijava_upload1", null);
+            testWiki.getImage("PIA21465 - North Polar Layers.jpg", expected);
+            String description = "Test image. Source (PD-NASA): [[:File:PIA21465 - North Polar Layers.jpg]]. ∑∑ƒ∂ß";
+            String reason = "Testing upload. ∑∑ƒ∂ß";
+            String uploadDest = "Wiki.java test4.jpg";
+            testWiki.upload(expected, uploadDest, description, reason);
+            // verify file uploaded is identical to image
+            File actual = File.createTempFile("wikijava_upload2", null);
+            testWiki.getImage(uploadDest, actual);
+            assertArrayEquals("upload: image", Files.readAllBytes(expected.toPath()), 
+                Files.readAllBytes(actual.toPath()));
+            assertEquals("upload: description", description, testWiki.getPageText("File:" + uploadDest));
+            assertEquals("upload: reason", reason, testWiki.getTopRevision("File:" + uploadDest).getComment());
+
+            // upload via URL
+            // target: https://test.wikipedia.org/wiki/File:Wiki.java_test5.jpg
+            // image: https://commons.wikimedia.org/wiki/File:(Tsander)_Large_Impact_Crater,_Lunar_Surface.jpg (1.55 MB)
+            description = "Test image. Source (PD-NASA): [[:File:(Tsander) Large Impact Crater, Lunar Surface.jpg]]";
+            uploadDest = "Wiki.java test5.jpg";
+            reason = "Testing upload via URL";
+            testWiki.upload(new URL("https://upload.wikimedia.org/wikipedia/commons/b/bc/%28Tsander%29_Large_Impact_Crater%2C_Lunar_Surface.jpg"), 
+                uploadDest, description, reason);
+            // verify file uploaded is identical to copied image
+            expected = File.createTempFile("wikijava_upload3", null);
+            testWiki.getImage("(Tsander) Large Impact Crater, Lunar Surface.jpg", expected);
+            actual = File.createTempFile("wikijava_upload4", null);
+            testWiki.getImage(uploadDest, actual);
+            // 1.55 MB file
+            assertArrayEquals("upload via url: image", Files.readAllBytes(expected.toPath()), 
+                Files.readAllBytes(actual.toPath()));
+            assertEquals("upload via url: description", description, testWiki.getPageText("File:" + uploadDest));
+            assertEquals("upload via url: reason", reason, testWiki.getTopRevision("File:" + uploadDest).getComment());
+        }
+        finally
+        {
+            // Requires admin rights... otherwise find an admin to delete manually.
+            testWiki.delete("File:Wiki.java test4.jpg", "Test cleanup");
+            testWiki.delete("File:Wiki.java test5.jpg", "Test cleanup");
+        }
     }
     
     @Test
@@ -91,13 +121,5 @@ public class LoggedInUnitTests
         testWiki.edit(page, text, summary);
         assertEquals("edit: page text", text, testWiki.getPageText(page));
         assertEquals("edit: summary", summary, testWiki.getTopRevision(page).getComment());
-    }
-    
-    @AfterClass
-    public static void cleanup() throws Exception
-    {
-        // Requires admin rights... otherwise find an admin to delete manually.
-        testWiki.delete("File:Wiki.java test4.jpg", "Test cleanup");
-        testWiki.delete("File:Wiki.java test5.jpg", "Test cleanup");
     }
 }